78cfca1beb97958f9c495cb2648e8adda087fcb0
Kingsley Abonyi <52834209+kingsleyabonyi@users.noreply.github.com>
2023-02-17 05:47:11 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,52 +1,37 @@
-import sys  
-import re  # regular expression module
+#!/usr/bin/python3
+'''A script that reads stdin line by line and computes metrics'''
 
 
-def log_parsing():
-    """gets infomation from logs"""
-    codes = [200, 301, 400, 401, 403, 404, 405, 500] # possible status codes to be present in logs
-    
-    # created a dictionary to count the noof occurrence of each status code
-    status_count = {status: 0 for status in codes}
-    
-    # stores the file size of each log
-    file_size = []
-    
-    # count will be used to count the no of lines
-    count = 0
-    status = None
-    try:
-        # loop through the standard input
-        for line in sys.stdin:
-            count += 1
-            search = re.search('GET .* (.*) (.*)', line) # capture the status code and file size of a log with regex
-            
-            status = int(search.group(1))  # assign the status code
-            
-            if status in status_count.keys(): # check if the status code is in the allowed status codes
-            
-                status_count[status] += 1    # if yes, increase the count of the status code
-                
-                file_size.append(int(search.group(2)))   # append the file size of that log to file_size list
-                
-            if count == 10:   # check if the no lines == 10
-                count = 0     # set count back to zero
-                
-                print("File size: {}".format(sum(file_size))) # if yes, print the file size and the no of occurrence of each \n
-                for key, val in status_count.items():         # status code so far
-                    if val != 0:
-                        print("{}: {}".format(key, val))
-        print("File size: {}".format(sum(file_size)))   # same as the print statement above but caters for when the loop and ended 
-        for key, val in status_count.items():           # example: lets say the no of logs is 31, this would cater for the last log
-            if val != 0:                                # since "if count == 10:" would have happend to be true 3 times
-                print("{}: {}".format(key, val))
-    except KeyboardInterrupt:
-        print("File size: {}".format(sum(file_size)))  # same as above but for if KeyboardInterrupt is encountered
-        for key, val in status_count.items():
-            if val != 0:
-                print("{}: {}".format(key, val))
-        raise
+import sys
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-if __name__ == "__main__":
-    log_parsing()
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
+
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
